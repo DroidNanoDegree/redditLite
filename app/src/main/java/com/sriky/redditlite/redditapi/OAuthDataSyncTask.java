556a4299e595dbebee26f4e4484b015d449f072f
sriky <srikanthbm@gmail.com>
2017-12-25 20:01:36 -0800

    Refactor: Move Reddit API classes to the appropriately named package.

--- app/src/main/java/com/sriky/redditlite/redditapi/OAuthDataSyncTask.java
+++ app/src/main/java/com/sriky/redditlite/redditapi/OAuthDataSyncTask.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and limitations under the License.
  */
 
-package com.sriky.redditlite.tokenstore;
+package com.sriky.redditlite.redditapi;
 
 import android.content.ContentProviderOperation;
 import android.content.Context;
