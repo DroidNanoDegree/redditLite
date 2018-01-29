e8af2999f9c68e91cf941d635177b598ef3ef88e
sriky <srikanthbm@gmail.com>
2018-01-29 09:09:54 -0800

    Refactor: remove unused events

--- app/src/main/java/com/sriky/redditlite/event/Message.java
+++ app/src/main/java/com/sriky/redditlite/event/Message.java
@@ -15,58 +15,10 @@
 
 package com.sriky.redditlite.event;
 
-import android.content.Context;
-
-import net.dean.jraw.RedditClient;
-import net.dean.jraw.oauth.AccountHelper;
-
 /**
  * Class for all {@link org.greenrobot.eventbus.EventBus} messages.
  */
 
 public final class Message {
 
-    /**
-     * Message sent after initializing the {@link RedditClient} via
-     * {@link com.sriky.redditlite.redditapi.ClientManager#requestRedditAccountHelper(Context, int)} call.
-     */
-    public static class OnRedditClientManagerRequestCompleted {
-        private int mRequestCode;
-        private AccountHelper mAccountHelper;
-
-        public OnRedditClientManagerRequestCompleted(int requestCode, AccountHelper accountHelper) {
-            mAccountHelper = accountHelper;
-            mRequestCode = requestCode;
-        }
-
-        /**
-         * The request code that was passed when {@link RedditClient} was requested via
-         * {@link com.sriky.redditlite.redditapi.ClientManager#requestRedditAccountHelper(Context, int)}
-         *
-         * @return The requestCode.
-         */
-        public int getRequestCode() {
-            return mRequestCode;
-        }
-
-        /**
-         * Returns the {@link RedditClient} build via request to
-         * {@link com.sriky.redditlite.redditapi.ClientManager#requestRedditAccountHelper(Context, int)} or
-         *
-         * @return {@link RedditClient}
-         */
-        public RedditClient getRedditClient() {
-            return mAccountHelper.getReddit();
-        }
-
-        /**
-         * Returns the {@link AccountHelper} build via request to
-         * {@link com.sriky.redditlite.redditapi.ClientManager#requestRedditAccountHelper(Context, int)} call.
-         *
-         * @return {@link AccountHelper}
-         */
-        public AccountHelper getAccountHelper() {
-            return mAccountHelper;
-        }
-    }
 }
