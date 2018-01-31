52ebb2d604333a8aefe9a13b18055e70d416be62
sriky <srikanthbm@gmail.com>
2018-01-31 11:14:36 -0800

    Feat: Add Asynctask to perform authentication of the reddit client in the background.

---
+++ app/src/main/java/com/sriky/redditlite/redditapi/AuthenticationTask.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2018 Srikanth Basappa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package com.sriky.redditlite.redditapi;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import com.sriky.redditlite.event.Message;
+
+import net.dean.jraw.RedditClient;
+import net.dean.jraw.oauth.AuthManager;
+import net.dean.jraw.oauth.OAuthException;
+import net.dean.jraw.oauth.StatefulAuthHelper;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.lang.ref.WeakReference;
+
+import timber.log.Timber;
+
+/**
+ * Class to perform the authentication process in the background.
+ * <p>
+ * Triggers {@link Message.RedditClientAuthenticationComplete} event.
+ */
+
+public class AuthenticationTask extends AsyncTask<String, Void, Boolean> {
+    public static final int RESULT_AUTHENTICATION_FAILED = 100;
+
+    private static final int AUTHENTICATE = 1;
+    private static final int REAUTHENTICATE = 2;
+
+    private WeakReference<Context> mContext;
+    private StatefulAuthHelper mHelper;
+    private int mAuthenticationMode;
+
+    public AuthenticationTask(Context context, StatefulAuthHelper helper) {
+        mContext = new WeakReference<>(context);
+        mHelper = helper;
+        mAuthenticationMode = AUTHENTICATE;
+    }
+
+    public AuthenticationTask(Context context) {
+        mContext = new WeakReference<>(context);
+        mAuthenticationMode = REAUTHENTICATE;
+    }
+
+    @Override
+    protected Boolean doInBackground(String... params) {
+        switch (mAuthenticationMode) {
+            case AUTHENTICATE: {
+                try {
+                    RedditClient client = mHelper.onUserChallenge(params[0]);
+                    Timber.d("username:%s", client.me().getUsername());
+                    return true;
+                } catch (OAuthException e) {
+                    // Report failure if an OAuthException occurs
+                    Timber.e("Authentication error: " + e.getLocalizedMessage());
+                    return false;
+                }
+            }
+
+            case REAUTHENTICATE: {
+                String username = params[0];
+                //switch to "userless" mode.
+                if (AuthManager.USERNAME_USERLESS.equals(username)) {
+                    ClientManager.getRedditAccountHelper(mContext.get()).switchToUserless();
+                } else {
+                    ClientManager.getRedditAccountHelper(mContext.get()).switchToUser(username);
+                }
+                return true;
+            }
+
+            default:
+                throw new RuntimeException("Unsupported authentication mode: " + mAuthenticationMode);
+        }
+
+    }
+
+    @Override
+    protected void onPostExecute(Boolean result) {
+        switch (mAuthenticationMode) {
+            case AUTHENTICATE: {
+                // Finish the activity if it's still running
+                Activity host = (Activity) mContext.get();
+                if (host != null) {
+                    if (result) {
+                        host.setResult(result ? Activity.RESULT_OK : Activity.RESULT_CANCELED, new Intent());
+                    } else {
+                        host.setResult(RESULT_AUTHENTICATION_FAILED, new Intent());
+                    }
+                    host.finish();
+                }
+                break;
+            }
+
+            case REAUTHENTICATE: {
+                EventBus.getDefault().post(
+                        new Message.RedditClientAuthenticationComplete(result));
+                break;
+            }
+
+            default:
+                throw new RuntimeException("Unsupported authentication mode: " + mAuthenticationMode);
+        }
+    }
+}
